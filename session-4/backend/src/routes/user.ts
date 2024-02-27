6329a2b02d7f432957bb50c895fe6ecfdc4a7004
giripatel <giridharpatel143@gmail.com>
2024-02-27 22:25:12 +0530

    Create signup route

--- session-4/backend/src/routes/user.ts
+++ session-4/backend/src/routes/user.ts
@@ -2,18 +2,16 @@ import { Console, log } from 'console';
 import {Router,Request,Response} from 'express'
 import {mySchema} from '../zodvalidation/validation'
 import { createUser } from '../db/db';
-
+import { createAuthToken } from '../middlewares/createAuthToken';
 const userRouter: Router = Router();
 
 userRouter.post('/signup',async (req : Request,res :Response) => {
     
     const {userName , firstName , lastName, password} = req.body;
-    // const userDetails : Request = req.body;
 
     const balance : number = Math.floor(Math.random() * 1000)+1
     console.log(req.body)
     const userInputValidation = mySchema.safeParse(req.body);
-
 
     if(!userInputValidation.success) {
         res.status(401).json({
@@ -21,15 +19,22 @@ userRouter.post('/signup',async (req : Request,res :Response) => {
         })
         return;
     }
-    console.log('======================================== : ' + balance);
     
     const userCreate = await createUser(userName,firstName,lastName,password,balance);
-
-    // console.log(userCreate);
     
+    if(userCreate == null){
+        res.status(400).json({
+            message : "User with the username already exists"
+        })
+        return;
+    }
+
+    const authToken = createAuthToken(userName);
+
     console.log(userInputValidation)
     res.status(200).json({
-        message : "worked",
+        message : "User created successfully",
+        authToken : authToken
     });
 })
 
