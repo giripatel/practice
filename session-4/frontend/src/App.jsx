6e0c5b6d7088f0c5d5d6c409837b27f3238bdd85
Giridhar <giridharpatel143@gmail.com>
2024-03-01 23:49:18 +0530

    Add Signin page

--- session-4/frontend/src/App.jsx
+++ session-4/frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import Signup from './pages/Signup'
+import SignIn from './pages/SignIn'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
     <div className='flex justify-center items-center'>
-        <Signup></Signup>
+        {/* <Signup></Signup> */}
+        <SignIn></SignIn>
       </div>
     </>
   )
---
+++ session-4/frontend/src/pages/SignIn.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Button from '../components/Button'
+import Input from '../components/Input'
+const SignIn = () => {
+
+    return (
+        <div className='flex justify-center items-center w-full h-screen bg-gray-900'>
+            <div className='flex flex-col border-2 gap-1  border-black px-5 py-3 bg-gray-100 rounded-md justify-center items-center relative bottom-11'>
+                <h1 className='text-center my-4 text-2xl font-semibold '>Sign In</h1>
+                    <Input label='Email' className='p-1' height='h-10'/>
+                    <Input label='Passowrd' className='p-1' height='h-10'/>
+                    <Button  className='text-center mt-2 p-2' >Sign In</Button>
+            </div>
+        </div>
+    )
+}
+
+export default SignIn;
